3cac66f0eebb74097578bb2c7e8f3a530bbd05ac
Blitzbomber87 <elias.efly000@gmail.com>
2023-07-24 16:31:51 -0700

    Added code to script.js file

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 // Assignment code here
-
-
+//var password = document.getElementById("password");
+//var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//var passwordLength = 12;
+//var password = "";
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -14,4 +16,14 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function() {
+
+const userInput = prompt('Choose length of password between 8 - 128 characters');
+if (userInput >= 8 ) {
+  userInputTwo = prompt('Select character types to include in your password');
+}
+console.log(userInput);
+
+//if (response )
+});
